45a2d8b2f5c7ad5495e7ca821f1f9f7925bbecb9
VITORIA AZEVEDO DA CRUZ <23111769@dsn127m1246729.cfp127aluno.local>
2024-10-17 12:27:02 -0300

    feat:'mobile':q

--- mobileaquarela/app/src/main/java/br/senai/sp/jandira/mobile_aquarela/screens/EsqueceuSuaSenhaTela.kt
+++ mobileaquarela/app/src/main/java/br/senai/sp/jandira/mobile_aquarela/screens/EsqueceuSuaSenhaTela.kt
@@ -1,25 +1,68 @@
 package br.senai.sp.jandira.mobile_aquarela.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.mobile_aquarela.R
 
 @Composable
-fun EsqueceuSuaSenhaTela(){
+fun EsqueceuSuaSenhaTela() {
     Column {
-        Box {
-            Icon(imageVector = Icons.Default.ArrowBack, contentDescription = "")
+        Box{
+            Row {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack, contentDescription = "", modifier = Modifier.height(30.dp).width(30.dp).background(color = Color(0xff3E7D8D), shape = CircleShape).padding(4.dp), tint = Color.White
+                )
+            }
         }
-        Image(painter = painterResource(id = R.drawable.Logo1), contentDescription = " ")
-        Text(text = "Esqueceu sua senha?")
-        Text(text = "Enviaremos um e-mail com instruções de como redefinir sua senha")
-        OutlinedTextField(onValueChange = {}, value = " ")
+        Image(painter = painterResource(id = R.drawable.logo1), contentDescription = " ", modifier = Modifier
+            .height(80.dp)
+            .width(80.dp))
+        Text(text = "Esqueceu sua senha?", fontSize = 18.sp)
+        Text(text = "Enviaremos um e-mail com instruções de como redefinir sua senha", fontSize = 12.sp)
+        OutlinedTextField(onValueChange = {}, value = " ", colors = OutlinedTextFieldDefaults.colors(unfocusedContainerColor = Color(0xffB8CED4)), modifier = Modifier
+            .height(40.dp)
+            .width(260.dp), label = {Text(text = " ")})
+        Button(onClick = { /* TO DO */ }, modifier = Modifier
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xff3E7D8D),
+                        Color(0xff5DA5B7)
+                    )
+                ), shape = RoundedCornerShape(4.dp)
+            )
+            .height(40.dp)
+            .width(138.dp), colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)) {
+            Text(text = "Enviar")
+        }
     }
+}
+@Composable
+@Preview(showBackground = true)
+fun EsqueceuSuaSenhaPreview() {
+    EsqueceuSuaSenhaTela()
+}
